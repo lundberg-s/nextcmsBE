5d1f593d1470afe86f6081fd216ddcd16bbd5a68
mazmann <sadi.lundberg@hotmail.com>
2025-05-14 17:08:43 +0200

    update cookie settings for improved security

--- api/root/urls.py
+++ api/root/urls.py
@@ -1,29 +1,13 @@
-"""
-URL configuration for root project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
-from .views import CookieTokenObtainPairView, CookieTokenRefreshView, LogoutView
+from root.views import CookieTokenObtainPairView, CookieTokenRefreshView, CookieTokenVerifyView, LogoutView
 
 API_PREFIX = "api/"
 
 urlpatterns = [
     path(f"{API_PREFIX}login/", CookieTokenObtainPairView.as_view(), name="login"),
     path(f"{API_PREFIX}refresh/", CookieTokenRefreshView.as_view(), name="refresh_token"),
+    path(f"{API_PREFIX}verify/", CookieTokenVerifyView.as_view(), name="verify_token"),
     path(f"{API_PREFIX}logout/", LogoutView.as_view(), name="logout"),
     path(f"{API_PREFIX}admin/", admin.site.urls),
     path(f"{API_PREFIX}user/", include("user.urls")),
--- api/root/views.py
+++ api/root/views.py
@@ -1,5 +1,4 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -23,7 +22,7 @@ class CookieTokenObtainPairView(TokenObtainPairView):
             value=access_token,
             httponly=True,
             secure=True,
-            samesite="Lax",
+            samesite="Strict",
             max_age=300
         )
 
@@ -32,7 +31,7 @@ class CookieTokenObtainPairView(TokenObtainPairView):
             value=refresh_token,
             httponly=True,
             secure=True,
-            samesite="None",
+            samesite="Strict",
             max_age=604800
         )
 
@@ -41,35 +40,52 @@ class CookieTokenObtainPairView(TokenObtainPairView):
 
 class CookieTokenRefreshView(TokenRefreshView):
     def post(self, request, *args, **kwargs):
-        # Extract the refresh token from the cookie
         refresh_token = request.COOKIES.get('refresh_token')
 
         if not refresh_token:
             return Response({'error': 'Refresh token not found in cookies'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Inject refresh token into the request data
         request.data['refresh'] = refresh_token
 
-        # Call the parent method
         response = super().post(request, *args, **kwargs)
 
         if response.status_code != 200:
             return response
 
-        # Extract new access token
         access_token = response.data.get("access")
 
         res = Response({'message': 'Token refreshed'}, status=status.HTTP_200_OK)
         res.set_cookie(
             key="access_token",
             value=access_token,
-            httponly=False,
-            secure=True,  # Set to False for local dev (True for production with HTTPS)
-            samesite="None",
+            httponly=True,
+            secure=True, 
+            samesite="Strict",
             max_age=300
         )
 
         return res
+
+
+class CookieTokenVerifyView(TokenVerifyView):
+    def post(self, request, *args, **kwargs):
+        print(request.data)
+        access_token = request.COOKIES.get('access_token')
+
+        if not access_token:
+            return Response({'error': 'Access token not found in cookies'}, 
+                           status=status.HTTP_401_UNAUTHORIZED)
+
+        # Inject access token into the request data
+        request.data['token'] = access_token
+
+        # Call the parent method
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == 200:
+            return Response({'message': 'Token is valid'}, status=status.HTTP_200_OK)
+        
+        return response
 
 
 class LogoutView(APIView):
