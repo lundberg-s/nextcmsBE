6e52dcc141666a43d48d29bbdf81d01e7095129b
Sadi Lundberg <sadi.lundberg@hotmail.com>
2025-05-23 18:49:17 +0200

    refactor: move token details into a utility function

--- api/root/views.py
+++ api/root/views.py
@@ -3,95 +3,64 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
+from root.utilities import (
+    set_access_token_cookie,
+    set_refresh_token_cookie,
+    delete_token_cookies,
+)
+
 class CookieTokenObtainPairView(TokenObtainPairView):
     def post(self, request, *args, **kwargs):
-
-        response = super().post(request, *args, **kwargs)
-
-        if response.status_code != 200:
-            return response
-
-        data = response.data
-        access_token = data.get("access")
-        refresh_token = data.get("refresh")
-
-        res = Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
-
-        res.set_cookie(
-            key="access_token",
-            value=access_token,
-            httponly=True,
-            secure=True,
-            samesite="Strict",
-            max_age=300
-        )
-
-        res.set_cookie(
-            key="refresh_token",
-            value=refresh_token,
-            httponly=True,
-            secure=True,
-            samesite="Strict",
-            max_age=604800
-        )
-
-        return res
-
-
-class CookieTokenRefreshView(TokenRefreshView):
-    def post(self, request, *args, **kwargs):
-        refresh_token = request.COOKIES.get('refresh_token')
-
-        if not refresh_token:
-            return Response({'error': 'Refresh token not found in cookies'}, status=status.HTTP_400_BAD_REQUEST)
-
-        request.data['refresh'] = refresh_token
-
         response = super().post(request, *args, **kwargs)
 
         if response.status_code != 200:
             return response
 
         access_token = response.data.get("access")
+        refresh_token = response.data.get("refresh")
 
-        res = Response({'message': 'Token refreshed'}, status=status.HTTP_200_OK)
-        res.set_cookie(
-            key="access_token",
-            value=access_token,
-            httponly=True,
-            secure=True, 
-            samesite="Strict",
-            max_age=300
-        )
+        res = Response(status=status.HTTP_200_OK)
+        set_access_token_cookie(res, access_token)
+        set_refresh_token_cookie(res, refresh_token)
 
         return res
 
+class CookieTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get('refresh_token')
+
+        if not refresh_token:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        request.data['refresh'] = refresh_token
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code != 200:
+            return response
+
+        access_token = response.data.get("access")
+        res = Response(status=status.HTTP_200_OK)
+        set_access_token_cookie(res, access_token)
+
+        return res
 
 class CookieTokenVerifyView(TokenVerifyView):
     def post(self, request, *args, **kwargs):
-        print(request.data)
         access_token = request.COOKIES.get('access_token')
 
         if not access_token:
-            return Response({'error': 'Access token not found in cookies'}, 
-                           status=status.HTTP_401_UNAUTHORIZED)
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-        # Inject access token into the request data
         request.data['token'] = access_token
-
-        # Call the parent method
         response = super().post(request, *args, **kwargs)
 
         if response.status_code == 200:
-            return Response({'message': 'Token is valid'}, status=status.HTTP_200_OK)
-        
-        return response
+            return Response(status=status.HTTP_200_OK)
 
+        return response
 
 class LogoutView(APIView):
     def post(self, request):
         res = Response({'message': 'Logged out'})
-        res.delete_cookie('access_token')
-        res.delete_cookie('refresh_token')
+        delete_token_cookies(res)
         return res
-    
